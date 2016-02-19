a2a742f88aab5a5727e69fc0938a5a7006f876eb
saikrishna <saikrishna@saikrishnas-mbp.infinio.co>
2016-02-19 11:10:38 +0530

    Initial Commit

---
+++ RestoApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RestoApp
+//
+//  Created by saikrishna on 19/02/16.
+//  Copyright © 2016 saikrishna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
